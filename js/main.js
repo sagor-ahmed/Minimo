4ee4f2e47965268c41e91c7df34b858fae12e15b
sagor-ahmed <sagorahmed00999@gmail.com>
2021-04-04 05:58:52 +0000

    Minimo Blog Template

---
+++ js/main.js
@@ -0,0 +1,79 @@
+(function ($) {
+
+
+
+    ////////////////////////////////////////////////////
+
+    // 0. Scroll To Top Js
+
+    function smoothSctollTop() {
+
+        $('.smooth-scroll a').on('click', function (event) {
+
+            var target = $(this.getAttribute('href'));
+
+            if (target.length) {
+
+                event.preventDefault();
+
+                $('html, body').stop().animate({
+
+                    scrollTop: target.offset().top - 0
+
+                }, 1500);
+
+            }
+
+        });
+
+    }
+
+    smoothSctollTop();
+
+
+
+    // Show or hide the sticky footer button
+
+    $(window).on('scroll', function (event) {
+
+        if ($(this).scrollTop() > 600) {
+
+            $('#scroll').fadeIn(200)
+
+        } else {
+
+            $('#scroll').fadeOut(200)
+
+        }
+
+    });
+
+
+
+    //Animate the scroll to yop
+
+    $('#scroll').on('click', function (event) {
+
+        event.preventDefault();
+
+
+
+        $('html, body').animate({
+
+            scrollTop: 0,
+
+        }, 1500);
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+})(jQuery);
